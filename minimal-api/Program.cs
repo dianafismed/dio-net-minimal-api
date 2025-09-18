d308eb372c517d630ab8d13e336d47c1f1d2914d
Diana Avila <missernightangel@gmail.com>
2025-09-17 21:43:46 -0300

    Pugins

--- minimal-api/Program.cs
+++ minimal-api/Program.cs
@@ -1,6 +1,41 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+  c.SwaggerDoc("v1", new OpenApiInfo { Title = "PizzaStore API", Description = "Making the Pizzas you love", Version = "v1" });
+});
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI(c =>
+  {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "PizzaStore API V1");
+  });
+}
 
 app.MapGet("/", () => "Hello World!");
 
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+  if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123")
+  {
+    return Results.Ok("Login Efetuado");
+  }
+  return Results.Unauthorized();
+});
+
 app.Run();
+
+
+public class LoginDTO
+{
+  public string Email { get; set; } = default!;
+  public string Senha { get; set; } = default!;
+}
+
+
